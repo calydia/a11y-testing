5ad354a5b2b5f0d0753dfd7acb72152b75dea047
Sanna Mäkinen <sanna.makinen@gmail.com>
2023-04-10 20:54:43 +0300

    Favicons and other niceties

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,81 @@ module.exports = {
       __key: "images"
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Testing for accessibility",
+        short_name: "A11y testing",
+        start_url: "/",
+        background_color: "#FFFFFF",
+        theme_color: "#BBC9F7",
+        display: "standalone",
+        icons: [
+          {
+            "src": "/favicons/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicons/android-chrome-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicons/apple-touch-icon.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicon/icon-16x16.png",
+            "sizes": "16x16",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicon/icon-32x32.png",
+            "sizes": "32x32",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicon/icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicon/icon-78x78.png",
+            "sizes": "78x78",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicon/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicon/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicon/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicon/maskable.png",
+            "sizes": "196x196",
+            "type": "image/png",
+            "purpose": "any maskable"
+          }
+        ],
+        crossOrigin: `use-credentials`,
+      },
+    },
+    {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `grmu7gmzjx8f`,
