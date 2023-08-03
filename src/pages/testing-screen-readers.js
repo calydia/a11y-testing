144d2a40644dfeb2c4ec93cfc0dbd301567b86c1
Sanna Mäkinen <sanna.makinen@gmail.com>
2023-08-03 13:45:29 +0300

    Add a dot after a sentence

--- src/pages/testing-screen-readers.js
+++ src/pages/testing-screen-readers.js
@@ -101,7 +101,7 @@ const ScreenReaderPage = () => {
 
             <h2>Language</h2>
 
-            <p>The page language must be correct, CMS like Drupal usually handles it automatically. But for any text in another language, you need to specify the language with the lang attribute. <span lang="fi">Tämä teksti on suomea</span></p>
+            <p>The page language must be correct, CMS like Drupal usually handles it automatically. But for any text in another language, you need to specify the language with the lang attribute. <span lang="fi">Tämä teksti on suomea</span>.</p>
             <p>When the language is marked correctly, the screen readers can read it correctly. Note that some screen readers require you to install language packages separately. So if your screen reader doesn't switch language, that might be the reason.</p>
           </div>
         </div>
