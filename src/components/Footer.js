fba324835176171c6bc71f8d4938e58a1109e39f
Sanna Kramsi <sanna.kramsi@gmail.com>
2024-03-24 17:03:50 +0200

    Name change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="link-wrapper mx-auto my-0">
-        <span style={spanStyle}><a className="border-solid border-transparent border-b-2 border-t-2 block py-1.5 text-lg" href="https://sanna.ninja">Sanna Mäkinen</a></span>
+        <span style={spanStyle}><a className="border-solid border-transparent border-b-2 border-t-2 block py-1.5 text-lg" href="https://sanna.ninja">Sanna Kramsi</a></span>
         <a className="border-solid border-transparent border-b-2 border-t-2 block py-1.5 text-lg" href="https://a11y.sanna.ninja" aria-label="Learn more about accessibility on my accessibility site">My accessibility site</a>
       </div>
     </footer>
